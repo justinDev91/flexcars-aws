1f0d6731c4470d97388e81e3449b4525b5580275
Justin katasi <justin.katasi@capgemini.com>
2025-06-12 16:46:14 +0200

    feat(front-admin): delete unused files

--- src/flexcars-frontend/app/(authed)/chat/chat-1/ConnectionState.jsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ConnectionState({ isConnected }) {
-  return <p className="flex items-start justify-start p-4">State: {"" + isConnected}</p>;
-}
--- src/flexcars-frontend/app/(authed)/chat/chat-2/ConnectionState.jsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function ConnectionState({ isConnected }) {
-  return <p className="flex items-start justify-self-end p-4">State: {"" + isConnected}</p>;
-}
