3f806b39b2f85390b0a319c010a6e38d8b329703
StephanG2a <stephan.gueorguieff@gmail.com>
2025-07-11 19:50:33 +0200

    feat(backend): implement carsitter validation system

--- src/flexcars-backend/src/carsitter/cars-sitter.module.ts
+++ src/flexcars-backend/src/carsitter/cars-sitter.module.ts
@@ -1,12 +1,15 @@
 
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 import { PrismaService } from 'src/prisma.service';
 import { CarSitterController } from './cars-sitter.controller';
 import { CarSitterService } from './cars-sitter.service';
 
 @Module({
+  imports: [MailerModule],
   controllers: [CarSitterController],
   providers: [CarSitterService, PrismaService],
+  exports: [CarSitterService],
 })
 export class CarSitterModule {}
